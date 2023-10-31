20546c7699f1cd15261888027d39fe72fbd7caf2
Debabrata Mondal <debabrata.js@protonmail.com>
2023-10-31 19:53:18 +0530

    remove caching

--- nextjs/src/app/api/initial/route.ts
+++ nextjs/src/app/api/initial/route.ts
@@ -1,6 +1,8 @@
 import { SERVER_URL } from "@/data/api"
 import { NextResponse } from "next/server";
 
+export const dynmic = 'force-dynamic'
+
 export const GET = async (req: Request, res: Response) => {
   const resp = await fetch(SERVER_URL + "/initial-questions");
   const data = await resp.json();
